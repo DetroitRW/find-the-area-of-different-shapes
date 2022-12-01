3109cabb2b1a041530c0148836328f899af71c91
Ilya Brant <detroitib31@gmail.com>
2022-12-01 16:52:07 +0600

    Написал код для ввода данных пользователем

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,6 @@ import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
    val reader = Scanner(System.`in`)
+    print("Круг(c), прямоугольник(r) или треугольник(t): ")
+    val f = reader.next()
 
