07df2c2379230d70aa6b1c00098439f2390b66da
Ilya Brant <detroitib31@gmail.com>
2022-12-01 16:54:27 +0600

    Написал код для выбора фигуры пользователем и ввод параметров фигур

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,3 +7,20 @@ fun main(args: Array<String>) {
     print("Круг(c), прямоугольник(r) или треугольник(t): ")
     val f = reader.next()
 
+    if (f == "c" ) {
+        print("Введите радиус: ")
+        val r = reader.nextDouble()
+        println("Площадь круга = ${circle(r)}")
+    } else if (f == "r") {
+        print("Введите 1 сторону: ")
+        val a = reader.nextDouble()
+        print("Введите 2 сторону: ")
+        val b = reader.nextDouble()
+        print("Площадь прямоугольника = ${rectangle(a, b)}")
+    } else if (f == "t") {
+        print("Введите 1 сторону: ")
+        var z = reader.nextDouble()
+        print("Введите 2 сторону: ")
+        var x = reader.nextDouble()
+        print("Введите 3 сторону: ")
+        var c = reader.nextDouble()
